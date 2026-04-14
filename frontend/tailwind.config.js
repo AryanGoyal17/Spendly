9c96f430f2fd7eab02a54cb5bb48ad4ec3ad4ebb
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-04-14 15:07:13 +0530

    chore: add tailwind and postcss configuration files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+//content -- includes the files which tailwind will scan to find class names
+
