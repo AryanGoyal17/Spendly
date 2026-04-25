6bab40cce976cb9cc486f43b4bf05cab5f3f0c78
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-04-25 17:33:08 +0530

    feat: implement bcrypt password hashing and validation hook

--- backend/models/User.js
+++ backend/models/User.js
@@ -36,4 +36,27 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true}); //timestamps = true -- automatically adds createdAt and updatedAt dates.. 
 
+//3-- Pre-Save hook (This fn runs before saving user to database--)
 
+userSchema.pre('save', async function(next) { // Run the async function before saving to database
+    
+    if(!this.isModified('password')) return next(); // If password is not modified, save without hashing...
+
+    const salt = await bcrypt.genSalt(10); // 10 = high complexity level (slower but safer)
+
+    this.password = await bcrypt.hash(this.password, salt); // replacing plane password with hashed password
+    
+    
+});
+    
+
+    userSchema.methods.matchPassword = async function(enteredPassword) { //Comparing entered password with stored hashed password
+        return await bcrypt.compare(enteredPassword, this.password);
+    };
+
+    const User = mongoose.model('User', userSchema); // Creating the user model. 
+    module.exports = User; // Making the model usable in other files .. 
+
+
+//The code which i have written above in User.js ensures that passwords are secretly hashed before saving in database and it 
+//also provides a method to verify passwords during login.. 
