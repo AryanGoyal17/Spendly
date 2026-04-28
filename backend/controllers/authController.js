4293bab1f652d9195351484c31379dd7fdf6d1ee
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-04-28 18:57:26 +0530

    implement secure JWT token generation for user authentication
    JWT secret key was generated using cryto(security library of Node

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,2 +1,15 @@
+//1--
+
 const User = require('../models/User') // Importing the User model so that backend can communicate with database 
 const jwt = require('jsonwebtoken') // Importing the jwt package
+
+//2-- Generating the jwt token..
+
+//NOTE: Used crypto(Node's built in security library) to generate the JWT_SECRET key and stored it in .env
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d', //Token expires in 30days, User has to login again after 30 days.. 
+    });
+};
+
